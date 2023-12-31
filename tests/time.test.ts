46f4da3dbbd53e83cf4a83b9e538d6f1a2b13606
Artiom Baloian <artiom.baloian@gmail.com>
2023-12-31 15:47:35 -0500

    Provided timeDiff() test cases

--- tests/time.test.ts
+++ tests/time.test.ts
@@ -1,5 +1,15 @@
 import { timeDiff } from '..';
 
 test('Testing timeDiff() function', () => {
-  expect(timeDiff(1702394102296, 1702404570878)).toBe('2 hours');
+  let start: number = 1702394102296;
+  let end: number = 1702404570878;
+  expect(timeDiff(start, end)).toBe('2 hours');
+
+  start = 1702394102180;
+  end = 1702395636391;
+  expect(timeDiff(start, end)).toBe('25 minutes');
+
+  start = 1702394102217;
+  end = 1702498818433;
+  expect(timeDiff(start, end)).toBe('1 day');
 });
