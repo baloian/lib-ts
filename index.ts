a30bd3bf8a9ac3341a2a103ce27a31edc30fd94e
Artiom Baloian <artiom.baloian@gmail.com>
2023-12-31 21:30:20 -0500

    Imported getRandomElemOfArray function

--- index.ts
+++ index.ts
@@ -3,4 +3,4 @@ export { timeDiff } from './lib/time';
 export { pctDiff, pctOf } from './lib/percentage';
 export { round, autoFixedDecimal } from './lib/numbers';
 export { abort } from './lib/abort';
-export { shuffle } from './lib/random';
+export { shuffle, getRandomElemOfArray } from './lib/random';
