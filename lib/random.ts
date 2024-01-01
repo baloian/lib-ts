19de3ccc1362b4f8fe866311e459adcebb950eb7
Artiom Baloian <artiom.baloian@gmail.com>
2023-12-31 21:23:24 -0500

    Implemented getRandomElemOfList function

--- lib/random.ts
+++ lib/random.ts
@@ -7,9 +7,17 @@ import cryptoNode from 'crypto';
 *
 * This function shuffles the given array in-place.
 */
-export function shuffle(array: any[]) {
+export function shuffle<T>(array: T[]) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = cryptoNode.randomInt(0, i + 1);
     [array[i], array[j]] = [array[j], array[i]];
   }
+}
+
+
+// Return random element (list[i]) of list such that 0 <= i < list.length.
+// The random implementation avoids modulo bias.
+export function getRandomElemOfList<T>(data: T[]): T | undefined {
+  if (data.length > 0) return data[cryptoNode.randomInt(0, data.length)];
+  return undefined;
 }
