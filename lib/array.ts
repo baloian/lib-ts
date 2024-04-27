ab067f9ea824555f7516f658176ba005cf22c1e9
Artiom Baloian <artiom.baloian@gmail.com>
2024-04-27 10:38:37 -0400

    Improved error message

--- lib/array.ts
+++ lib/array.ts
@@ -5,7 +5,7 @@ export function sumOfPropVal(list: unknown[], prop: string): number {
     if (e && typeof e === 'object' && e[prop] && typeof e[prop] === 'number') {
       total += e[prop];
     } else {
-      throw new Error(`list[${idx}] is not valid or does not have ${prop} property`);
+      throw new Error(`list[${idx}] is not valid or does not have the '${prop}' property`);
     }
   });
   return total;
