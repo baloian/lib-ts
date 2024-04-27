cd49df30147cfdeced07f90ac8a2559fd57e3475
Artiom Baloian <artiom.baloian@gmail.com>
2024-04-27 10:19:16 -0400

    Added lib/array.ts fiel and provided sumOfPropVal function

--- index.ts
+++ index.ts
@@ -4,3 +4,4 @@ export { pctDiff, pctOf } from './lib/percentage';
 export { round, autoFixedDecimal } from './lib/numbers';
 export { abort } from './lib/abort';
 export { shuffle, getRandomElemOfArray } from './lib/random';
+export { sumOfPropVal } from './lib/array';
---
+++ lib/array.ts
@@ -0,0 +1,12 @@
+// This function returns the sum of the property value in the array of objects.
+export function sumOfPropVal(list: unknown[], prop: string): number {
+  let total: number = 0;
+  list.forEach((e: unknown, idx: number) => {
+    if (e && typeof e === 'object' && e[prop] && typeof e[prop] === 'number') {
+      total += e[prop];
+    } else {
+      throw new Error(`list[${idx}] is not valid or does not have ${prop} property`);
+    }
+  });
+  return total;
+}
