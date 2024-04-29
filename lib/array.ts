a28478f4e8b8808774f94e74985b2af66a9edfc0
Artiom Baloian <artiom.baloian@gmail.com>
2024-04-29 15:02:52 -0400

    Minor fix

--- lib/array.ts
+++ lib/array.ts
@@ -2,7 +2,7 @@
 export function sumOfPropVal(list: unknown[], prop: string): number {
   let total: number = 0;
   list.forEach((e: unknown, idx: number) => {
-    if (e && typeof e === 'object' && e[prop] && typeof e[prop] === 'number') {
+    if (e && typeof e === 'object' && e[prop] !== undefined && typeof e[prop] === 'number') {
       total += e[prop];
     } else {
       throw new Error(`list[${idx}] is not valid or does not have the '${prop}' property`);
